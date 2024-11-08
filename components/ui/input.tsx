75902854167e8c6b86f6112efc9de55aa55493a9
Ryan T. Murphy <pianofreq@gmail.com>
2024-11-08 00:24:58 -0500

    fix: import statements for input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,10 @@
 import * as React from "react"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-import { cn } from "@/lib/utils"
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+} 
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
