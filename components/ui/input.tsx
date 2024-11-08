07de2786980466c95071a9a9c1ccf6d064af38c6
Ryan T. Murphy <pianofreq@gmail.com>
2024-11-08 00:37:17 -0500

    fix: https://stackoverflow.com/questions/79046700/an-interface-declaring-no-members-is-equivalent-to-its-supertype-typescript-es

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -6,8 +6,8 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 } 
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
